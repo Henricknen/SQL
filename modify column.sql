74ea325a89feaeb0966ebb4ebe12e296764ec2eb
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-03-13 22:05:30 -0300

    exclusao_da_coluna_'i_idade_cliente'_da_tabela_'cliente'

--- modify column.sql
+++ modify column.sql
@@ -1,3 +1,5 @@
 ALTER TABLE cliente ADD	i_idade_cliente INT;		/* adicionando 'ADD' coluna na tabela 'cliente' */
 
 ALTER TABLE cliente MODIFY COLUMN i_idade_cliente TINYINT;		/* modificndo_'i_idade_cliente'_de_'INT'_para_'TINYINT' */
+
+ALTER TABLE cliente DROP COLUMN i_idade_cliente;		/* excluindo_coluna_'i_idade_cliente'_da_tabela_'cliente' */
