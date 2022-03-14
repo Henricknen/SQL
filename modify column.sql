6ea8763bbe106c4f30882e23239793bc9874bcfe
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-03-13 21:53:20 -0300

    comando_ultilizado_para_modificar_o_tipo_de_dados_de_uma_coluna_e_descartar_uma_coluna_existente

---
+++ modify column.sql
@@ -0,0 +1,3 @@
+ALTER TABLE cliente ADD	i_idade_cliente INT;		/* adicionando 'ADD' coluna na tabela 'cliente' */
+
+ALTER TABLE cliente MODIFY COLUMN i_idade_cliente TINYINT;		/* modificndo_'i_idade_cliente'_de_'INT'_para_'TINYINT' */
