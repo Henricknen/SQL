0d359288b82a9259c647395801c596ae65edf397
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-05-20 10:46:38 -0300

    adicionando_restricao_de_chave_estrangeira

--- foreign key.sql
+++ foreign key.sql
@@ -1,11 +1,14 @@
+									/* DDL - Data Definition Language */
 
 CREATE TABLE tipocliente(
-	i_cliente_tipocliente INT PRIMARY KEY AUTO_INCREMENT,
+	i_tipocliente_tipocliente INT PRIMARY KEY AUTO_INCREMENT,
     s_dsctipocliente_tipocliente VARCHAR(100) NOT NULL
 );
 
-/* alter table <tabela_origem> add constraint <nome_restricao> foreign key (<campo_tabela_origem>) references <tabela_destino> (<campo_tabela_destino>); */
-ALTER TABLE cliente ADD CONSTRAINT fk_tipocliente FOREIGN KEY (i_tipo_cliente) REFERENCES tipocliente (i_cliente_tipocliente); /* criando uma restricao 'chave estrangeira' na tabela cliente */
+												/* adição de restrição */
+ ALTER TABLE cliente ADD CONSTRAINT fk_tipocliente FOREIGN KEY (i_tipo_cliente) REFERENCES tipocliente (i_tipocliente_tipocliente); /* criando uma restricao 'chave estrangeira' na tabela cliente */
+/*ALTER TABLE <tabela_origem> ADD CONSTRAINT <nome_restricao> FOREIGN KEY (<campo_tabela_origem>) REFERENCES <tabela_destino> (<campo_tabela_destino>); */
+
 
 
 
