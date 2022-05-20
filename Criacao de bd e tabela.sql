995950c8b5a4858b057ef2cc56df3abcac2e4d7a
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-05-20 00:37:36 -0300

    criacao_de_um_banco_de_dados_e_tabela

--- Criacao de bd e tabela.sql
+++ Criacao de bd e tabela.sql
@@ -1,11 +1,12 @@
-CREATE SCHEMA bd_lhsf;		/* criacao do banco de dados 'bd_lhsf'  */
+									/* DDL - Data Definition Language */
+                                    
+CREATE SCHEMA  bd_lhsf;		/* criacao do banco de dados 'bd_lhsf'  */
 
 USE bd_lhsf;		/* selecionado o banco de dados 'bd_lhsf */
 
-CREATE TABLE cliente(		/* criacao da tabela 'cliente'*/
-	i_cliente_cliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    s_nome_cliente VARCHAR(50) NOT NULL,
-    s_cpf_cliente VARCHAR(11) NOT NULL,
+CREATE TABLE cliente(	/* criacao da tabela 'cliente'*/
+	i_cliente_cliente INT PRIMARY KEY NOT NULL AUTO_INCREMENT,		/* Tipo de dados/ Nome da coluna/ Nome da tabela */
+    s_nome_cliente VARCHAR(50) NOT NULL,		/* é dinamico pode ocupar até 50, mas se o nome tiver 20 caracter o campo so terá 20 caracteres de armazenamento */
+    s_cpf_cliente CHAR(11) NOT NULL,		/* tem tamanho fixo o campo terá 11 caracteres */
     d_nasc_cliente DATETIME 
 );
-
