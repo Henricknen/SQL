51c35c8dcae4eb1ae7aee08367f8133849f6c640
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-05-20 22:14:56 -0300

    adicionando_coluna_'i_idade_cliente'_na_tabela_'cliente'_modificando-a_seu_tupo_e_excluindo

--- modify column.sql
+++ modify column.sql
@@ -1,5 +1,7 @@
-ALTER TABLE cliente ADD	i_idade_cliente INT;		/* adicionando 'ADD' coluna na tabela 'cliente' */
+									/* DDL - Data Definition Language */
+                                    
+ALTER TABLE cliente ADD	i_idade_cliente INT;		/* adicionando coluna i_idade_cliente na tabela cliente */
 
-ALTER TABLE cliente MODIFY COLUMN i_idade_cliente TINYINT;		/* modificndo_'i_idade_cliente'_de_'INT'_para_'TINYINT' */
+ALTER TABLE cliente MODIFY COLUMN i_idade_cliente TINYINT;		/* modificando i_idade_cliente de INT para TINYINT */
 
-ALTER TABLE cliente DROP COLUMN i_idade_cliente;		/* excluindo_coluna_'i_idade_cliente'_da_tabela_'cliente' */
+ALTER TABLE cliente DROP COLUMN i_idade_cliente;		/* excluindo coluna i_idade_cliente da tabela cliente */
