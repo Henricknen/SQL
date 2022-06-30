c4bf65d0c7d2e98211aed6825bc3e7d41ee9a02e
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-06-30 18:25:18 -0300

    fazendo_a_ordenacao_dos_registros_retornados_pela_consulta_com_a_clausula_'order_by'

---
+++ order_by.sql
@@ -0,0 +1,9 @@
+											  /* ORDER BY */
+
+SELECT
+    * 
+FROM
+	cliente
+ORDER BY		/* vai ser feita a ordenação da colunas abaixo */
+	i_tipo_cliente, 1		/* pode ser indicado nome ou posição das colunas  */
+;
